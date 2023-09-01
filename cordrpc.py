a2faa2137183deb09e5a6b9e0514fa7bb8cc7772
Abbas Lutvaliyev <91781891+z3ven@users.noreply.github.com>
2023-09-01 21:20:44 +0400

    CordRPC 2.0
    - GUI
    - Updated error handler

--- cordrpc.py
+++ cordrpc.py
@@ -1,31 +1,73 @@
 import json
 import pypresence
-import pyfiglet
-import colorama
 import time
-import sys
-if __name__ == "__main__":
-    colorama.init()
-    print(pyfiglet.figlet_format("CordRPC", "standard"))
+import customtkinter as ctk
+from tkinter import messagebox
+def poll():
     try:
-        config = json.loads(open("config.json", "r").read())
-        start = int(time.time())
-        RPC = pypresence.Presence(config["client_id"])
+        RPC = pypresence.Presence(client_id.get())
         RPC.connect()
+        start = int(time.time())
+
         while True:
-            print("Polling \n")
+            btns = [
+                {
+                    "label": btn1_label.get(),
+                    "url": btn1_url.get()
+                },
+                {
+                    "label": btn2_label.get(),
+                    "url": btn2_url.get()
+                }
+            ]
             RPC.update(
-                large_image=config["large_image"],
-                large_text=config["large_text"],
-                details=config["details"],
-                state=config["state"],
-                start=start,
-                buttons=config["buttons"]
+            large_image=large_image.get(),
+            large_text=large_text.get(),
+            details=details.get(),
+            state=state.get(),
+            start=start,
+            buttons=btns
             )
-            time.sleep(config["update_time"])
-    except FileNotFoundError:
-        print(f"{colorama.Fore.RED}cordrpc: Could not find config.json.")
-    except KeyboardInterrupt:
-        sys.exit()
+            time.sleep(int(update_time.get()))
     except Exception as e:
-        print(f"{colorama.Fore.RED}cordrpc: {str(e)}")
+        messagebox.showerror("An error occued", message=f"An error occued: {str(e)}")
+root = ctk.CTk()
+root.title("CordRPC")
+l = ctk.CTkLabel(root, text="Welcome to CordRPC!")
+client_id = ctk.CTkEntry(root, width=250, placeholder_text="Client ID")
+large_image = ctk.CTkEntry(root, width=250, placeholder_text="Large Image")
+large_text = ctk.CTkEntry(root, width=250, placeholder_text="Large text")
+details = ctk.CTkEntry(root, width=250, placeholder_text="Details")
+state = ctk.CTkEntry(root, width=250, placeholder_text="State")
+update_time = ctk.CTkEntry(root, width=250, placeholder_text="Update Time")
+button_section = ctk.CTkLabel(root, text="Buttons: ")
+btn1_section = ctk.CTkLabel(root, text="Button 1: ")
+btn1_label = ctk.CTkEntry(root, placeholder_text="Label", width=250)
+btn1_url = ctk.CTkEntry(root, placeholder_text="URL", width=250)
+btn2_section = ctk.CTkLabel(root, text="Button 2: ")
+btn2_label = ctk.CTkEntry(root, placeholder_text="Label", width=250)
+btn2_url = ctk.CTkEntry(root, placeholder_text="URL", width=250)
+start_btn = ctk.CTkButton(root, text="Start", command=poll)
+l.pack()
+client_id.pack()
+large_image.pack()
+large_text.pack()
+details.pack()
+state.pack()
+update_time.pack()
+button_section.pack()
+btn1_section.pack()
+btn1_label.pack()
+btn1_url.pack()
+btn2_section.pack()
+btn2_label.pack()
+btn2_url.pack()
+start_btn.pack()
+if __name__ == "__main__":
+    ctk.set_default_color_theme("dark-blue")
+    ctk.set_appearance_mode("dark")
+    root.mainloop() 
+
+    
+    
+        
